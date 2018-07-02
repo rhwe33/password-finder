033b56912496adf98657eb19a3f5437cca709458
rhwe33 <skorpeeon@protonmail.com>
2018-07-02 23:17:15 +0600

    Added moving border & new password generation

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,13 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+// Select random password
+function getRandomElement() {
+  var random = getRandomInt(1, 150);
+  return document.getElementsByClassName('password')[random];
+}
+
+// Generate password list
 for (var i = 1; i <= 150; i++) {
 
   var string = document.createElement('p');
@@ -19,3 +26,21 @@ for (var i = 1; i <= 150; i++) {
   }
 
 }
+
+document.onclick = function() {
+  var activePassword = document.getElementsByClassName('password active')[0];
+  var newPassword    = getRandomElement();
+
+  if (typeof(activePassword) != 'undefined' && activePassword != null) {
+    activePassword.classList.remove('active');
+  }
+  newPassword.className += ' active';
+
+  var coords   = newPassword.getBoundingClientRect();
+  var selector = document.getElementById('selector');
+
+  selector.style.left = coords.left + 'px';
+  selector.style.top  = coords.top + 'px';
+
+  console.log(selector.style.left);
+};
